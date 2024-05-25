fd1fef6749f52fc1030e6f5cc80afeda356d41ff
Daemon Jung <jjjssswww1006@gmail.com>
2024-05-26 00:10:43 +0900

    initial commit

---
+++ app/src/main/java/com/toyou/toyouandroid/ui/calendar/CalendarFragment.kt
@@ -0,0 +1,42 @@
+package com.toyou.toyouandroid.ui.calendar
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.toyou.toyouandroid.databinding.FragmentCalendarBinding
+
+class CalendarFragment : Fragment() {
+
+    private var _binding: FragmentCalendarBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val calendarViewModel =
+            ViewModelProvider(this).get(CalendarViewModel::class.java)
+
+        _binding = FragmentCalendarBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textCalendar
+        calendarViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/toyou/toyouandroid/ui/calendar/CalendarViewModel.kt
@@ -0,0 +1,13 @@
+package com.toyou.toyouandroid.ui.calendar
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CalendarViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is calendar Fragment"
+    }
+    val text: LiveData<String> = _text
+}
