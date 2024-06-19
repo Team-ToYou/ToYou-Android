b2c627bf61efe60750e79f3d03bb6fb66e741d00
jeong724 <oliveyou724@gmail.com>
2024-06-19 16:56:02 +0900

    [add] 카드 생성 ui

--- app/src/main/java/com/toyou/toyouandroid/ui/home/HomeFragment.kt
+++ app/src/main/java/com/toyou/toyouandroid/ui/home/HomeFragment.kt
@@ -7,11 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.toyou.toyouandroid.databinding.FragmentCreateBinding
 import com.toyou.toyouandroid.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentCreateBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,13 +26,13 @@ class HomeFragment : Fragment() {
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentCreateBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
+        /*val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
-        }
+        }*/
         return root
     }
 
