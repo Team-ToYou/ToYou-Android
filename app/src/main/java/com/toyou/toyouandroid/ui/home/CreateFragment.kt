1b0269b0b7ee41c6e6cb9b1c7657f7a621acab5d
jeong724 <oliveyou724@gmail.com>
2024-06-20 16:38:00 +0900

    [feat] 카드 생성 리사이클러뷰 아이템xml

--- app/src/main/java/com/toyou/toyouandroid/ui/home/CreateFragment.kt
+++ app/src/main/java/com/toyou/toyouandroid/ui/home/CreateFragment.kt
@@ -1,4 +1,24 @@
 package com.toyou.toyouandroid.ui.home
 
-class CreateFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.toyou.toyouandroid.databinding.FragmentCreateBinding
+
+class CreateFragment : Fragment(){
+
+    lateinit var binding : FragmentCreateBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCreateBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
 }
