45ca284f54c673259dc608e1025e792ab7009e15
jeong724 <oliveyou724@gmail.com>
2024-07-14 19:04:47 +0900

    [feat] SocialFragment

--- app/src/main/java/com/toyou/toyouandroid/ui/social/SocialFragment.kt
+++ app/src/main/java/com/toyou/toyouandroid/ui/social/SocialFragment.kt
@@ -1,6 +1,32 @@
 package com.toyou.toyouandroid.ui.social
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import com.toyou.toyouandroid.databinding.FragmentSocialBinding
 
 class SocialFragment : Fragment() {
+
+    private var _binding : FragmentSocialBinding? = null
+    private val binding : FragmentSocialBinding get() = requireNotNull(_binding){"social fragment null"}
+
+    lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSocialBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
 }
