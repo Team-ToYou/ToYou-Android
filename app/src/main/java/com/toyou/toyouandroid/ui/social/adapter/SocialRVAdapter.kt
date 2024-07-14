c1cc2e94813f0a5adffffa477409515e6adf4b88
jeong724 <oliveyou724@gmail.com>
2024-07-14 19:03:30 +0900

    [feat] 어댑터 코드 추가

--- app/src/main/java/com/toyou/toyouandroid/ui/social/adapter/SocialRVAdapter.kt
+++ app/src/main/java/com/toyou/toyouandroid/ui/social/adapter/SocialRVAdapter.kt
@@ -1,32 +1,42 @@
 package com.toyou.toyouandroid.ui.social.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.recyclerview.widget.RecyclerView
+import com.toyou.toyouandroid.R
 import com.toyou.toyouandroid.model.FriendListModel
 
 class SocialRVAdapter : RecyclerView.Adapter<SocialRVAdapter.SocialViewHolder>() {
+
+    private var friendList : List<FriendListModel> = emptyList()
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): SocialRVAdapter.SocialViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_friend_rv, parent, false)
+        return SocialViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: SocialRVAdapter.SocialViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(friendList[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return friendList.size
     }
 
     class SocialViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        fun bind(friend : FriendListModel){
+        private val friendName : TextView = itemView.findViewById(R.id.friendName_tv)
+        private val friendMessage : TextView = itemView.findViewById(R.id.friendMessage_tv)
 
+        fun bind(friend : FriendListModel){
+            friendName.text = friend.name
+            friendMessage.text = friend.message
         }
     }
 
