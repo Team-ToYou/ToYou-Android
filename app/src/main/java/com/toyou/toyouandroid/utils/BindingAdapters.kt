2c222a3a64aa925eac6a94b6a4cfb952c22c70b9
Daemon Jung <jjjssswww1006@gmail.com>
2024-07-15 10:01:53 +0900

    [feature] utils 이미지 비트맵 변환 함수 #13

--- app/src/main/java/com/toyou/toyouandroid/utils/BindingAdapters.kt
+++ app/src/main/java/com/toyou/toyouandroid/utils/BindingAdapters.kt
@@ -1,12 +1,24 @@
 package com.toyou.toyouandroid.utils
 
+import android.graphics.BitmapFactory
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.LiveData
+import com.toyou.toyouandroid.R
+import java.io.File
 
 @BindingAdapter("imageResource")
 fun setImageResource(imageView: ImageView, resource: LiveData<Int>?) {
     resource?.value?.let {
         imageView.setImageResource(it)
+    }
+}
+
+@BindingAdapter("imagePath")
+fun loadImage(view: ImageView, imagePath: String) {
+    val imgFile = File(imagePath)
+    if (imgFile.exists()) {
+        val bitmap = BitmapFactory.decodeFile(imgFile.absolutePath)
+        view.setImageBitmap(bitmap)
     }
 }
