d34a6180114c2aa4dae55b2682f81707a7adb922
Daemon Jung <jjjssswww1006@gmail.com>
2024-07-17 17:07:10 +0900

    [design] 캘린더 화면 틀 추가 #14

--- app/src/main/java/com/toyou/toyouandroid/presentation/fragment/calendar/CalendarFragment.kt
+++ app/src/main/java/com/toyou/toyouandroid/presentation/fragment/calendar/CalendarFragment.kt
@@ -4,35 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.toyou.toyouandroid.databinding.FragmentCalendarBinding
+import com.toyou.toyouandroid.databinding.FragmentMypageBinding
 
 class CalendarFragment : Fragment() {
 
     private var _binding: FragmentCalendarBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private val binding: FragmentCalendarBinding
+        get() = requireNotNull(_binding){"FragmentCalendarBinding -> null"}
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val calendarViewModel =
-            ViewModelProvider(this).get(CalendarViewModel::class.java)
 
         _binding = FragmentCalendarBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textCalendar
-        calendarViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
