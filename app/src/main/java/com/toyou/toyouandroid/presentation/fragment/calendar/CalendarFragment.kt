25527dc9622cee23575a0510ef3b2df051c812e6
Daemon Jung <jjjssswww1006@gmail.com>
2024-07-19 20:43:43 +0900

    [feature] 캘린더 친구 기록 리사이클러뷰 설정 #14

--- app/src/main/java/com/toyou/toyouandroid/presentation/fragment/calendar/CalendarFragment.kt
+++ app/src/main/java/com/toyou/toyouandroid/presentation/fragment/calendar/CalendarFragment.kt
@@ -5,11 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.GridLayoutManager
+import com.toyou.toyouandroid.R
 import com.toyou.toyouandroid.databinding.FragmentCalendarBinding
-import com.toyou.toyouandroid.databinding.FragmentMypageBinding
+import com.toyou.toyouandroid.model.CalendarItem
 
 class CalendarFragment : Fragment() {
 
+    lateinit var navController: NavController
     private var _binding: FragmentCalendarBinding? = null
     private val binding: FragmentCalendarBinding
         get() = requireNotNull(_binding){"FragmentCalendarBinding -> null"}
@@ -23,6 +28,48 @@ class CalendarFragment : Fragment() {
         _binding = FragmentCalendarBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+
+        val items = listOf(
+            CalendarItem(R.drawable.home_stamp_option_normal, "태연"),
+            CalendarItem(R.drawable.home_stamp_option_exciting, "승원"),
+            CalendarItem(R.drawable.home_stamp_option_happy, "현정"),
+            CalendarItem(R.drawable.home_stamp_option_anxiety, "유은"),
+            CalendarItem(R.drawable.home_stamp_option_normal, "태연"),
+            CalendarItem(R.drawable.home_stamp_option_upset, "승원"),
+            CalendarItem(R.drawable.home_stamp_option_exciting, "현정"),
+            CalendarItem(R.drawable.home_stamp_option_normal, "유은"),
+            CalendarItem(R.drawable.home_stamp_option_anxiety, "태연"),
+            CalendarItem(R.drawable.home_stamp_option_happy, "승원"),
+            CalendarItem(R.drawable.home_stamp_option_normal, "현정"),
+            CalendarItem(R.drawable.home_stamp_option_anxiety, "유은"),
+            CalendarItem(R.drawable.home_stamp_option_exciting, "태연"),
+            CalendarItem(R.drawable.home_stamp_option_normal, "태연"),
+            CalendarItem(R.drawable.home_stamp_option_exciting, "승원"),
+            CalendarItem(R.drawable.home_stamp_option_happy, "현정"),
+            CalendarItem(R.drawable.home_stamp_option_anxiety, "유은"),
+            CalendarItem(R.drawable.home_stamp_option_normal, "태연"),
+            CalendarItem(R.drawable.home_stamp_option_upset, "승원"),
+            CalendarItem(R.drawable.home_stamp_option_exciting, "현정"),
+            CalendarItem(R.drawable.home_stamp_option_normal, "유은"),
+            CalendarItem(R.drawable.home_stamp_option_anxiety, "태연"),
+            CalendarItem(R.drawable.home_stamp_option_happy, "승원"),
+            CalendarItem(R.drawable.home_stamp_option_normal, "현정"),
+            CalendarItem(R.drawable.home_stamp_option_anxiety, "유은"),
+            CalendarItem(R.drawable.home_stamp_option_exciting, "태연"),
+        )
+
+        val adapter = CalendarAdapter(items)
+        binding.calendarRv.layoutManager = GridLayoutManager(context, 6)
+        binding.calendarRv.adapter = adapter
+
+        val verticalSpaceHeight = resources.getDimensionPixelSize(R.dimen.recycler_item_spacing)
+        val horizontalSpaceHeight = verticalSpaceHeight / 2
+        binding.calendarRv.addItemDecoration(CalendarItemDecoration(horizontalSpaceHeight, verticalSpaceHeight))
     }
 
     override fun onDestroyView() {
