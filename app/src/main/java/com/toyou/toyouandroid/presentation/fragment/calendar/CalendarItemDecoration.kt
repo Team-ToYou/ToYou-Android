a893680120604cc1f4ca3713ffd7a49c1fa18648
Daemon Jung <jjjssswww1006@gmail.com>
2024-07-19 20:45:03 +0900

    [feature] 캘린더 친구 기록 리사이클러뷰 높이 제한 및 6열 설정에 따른 중앙정렬 #14

---
+++ app/src/main/java/com/toyou/toyouandroid/presentation/fragment/calendar/CalendarItemDecoration.kt
@@ -0,0 +1,13 @@
+package com.toyou.toyouandroid.presentation.fragment.calendar
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class CalendarItemDecoration(private val horizontalSpaceHeight: Int, private val verticalSpaceHeight: Int) : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        outRect.bottom = verticalSpaceHeight
+        outRect.right = horizontalSpaceHeight
+        outRect.left = horizontalSpaceHeight
+    }
+}
