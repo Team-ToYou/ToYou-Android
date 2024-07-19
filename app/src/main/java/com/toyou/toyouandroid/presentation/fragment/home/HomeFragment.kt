cd27b3e9ca7e6d7ed8d99d894957ccf29cecda26
Daemon Jung <jjjssswww1006@gmail.com>
2024-07-19 15:56:47 +0900

    [feature] 감정우표 선택값 viewmodel로 데이터 수신 #13

--- app/src/main/java/com/toyou/toyouandroid/presentation/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/toyou/toyouandroid/presentation/fragment/home/HomeFragment.kt
@@ -6,11 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.toyou.toyouandroid.R
 import com.toyou.toyouandroid.databinding.FragmentHomeBinding
+import com.toyou.toyouandroid.presentation.base.MainActivity
 import com.toyou.toyouandroid.presentation.viewmodel.HomeViewModel
 
 class HomeFragment : Fragment() {
@@ -19,7 +21,7 @@ class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding: FragmentHomeBinding
         get() = requireNotNull(_binding){"FragmentHomeBinding -> null"}
-    private val viewModel: HomeViewModel by viewModels()
+    private val viewModel: HomeViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,6 +30,8 @@ class HomeFragment : Fragment() {
     ): View {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
 
         // onBackPressedCallback 등록
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
@@ -43,12 +47,12 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
 
-        binding.viewModel = viewModel
-        binding.lifecycleOwner = viewLifecycleOwner
-
 //        viewModel.selectedStamp.observe(viewLifecycleOwner) { stamp ->
 //            (activity as MainActivity).setSelectedStamp(stamp)
 //        }
+
+        // MainActivity의 메소드를 호출하여 바텀 네비게이션 뷰 숨기기
+        (requireActivity() as MainActivity).hideBottomNavigation(false)
 
         // 우체통 클릭시 일기카드 생성 화면으로 전환(임시)
         binding.homeMailboxIv.setOnClickListener {
@@ -59,10 +63,29 @@ class HomeFragment : Fragment() {
             navController.navigate(R.id.action_navigation_home_to_notice_fragment)
         }
 
-        val bottomSheetFragment = HomeBottomSheetFragment()
-        bottomSheetFragment.show(parentFragmentManager, bottomSheetFragment.tag)
-    }
+        binding.homeEmotionIv.setOnClickListener {
+            navController.navigate(R.id.action_navigation_home_to_home_option_fragment)
+        }
 
+//        val bottomSheetFragment = HomeBottomSheetFragment()
+//        bottomSheetFragment.show(parentFragmentManager, bottomSheetFragment.tag)
+
+        viewModel.homeEmotion.observe(viewLifecycleOwner) { emotion ->
+            binding.homeEmotionIv.setImageResource(emotion)
+        }
+
+        viewModel.text.observe(viewLifecycleOwner) { text ->
+            binding.homeEmotionTv.text = text
+        }
+
+        viewModel.homeDateBackground.observe(viewLifecycleOwner) { date ->
+            binding.homeDateTv.setBackgroundResource(date)
+        }
+
+        viewModel.homeBackground.observe(viewLifecycleOwner) { background ->
+            binding.layoutHome.setBackgroundResource(background)
+        }
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/toyou/toyouandroid/presentation/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/toyou/toyouandroid/presentation/viewmodel/HomeViewModel.kt
@@ -3,23 +3,25 @@ package com.toyou.toyouandroid.presentation.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.toyou.toyouandroid.R
 
 class HomeViewModel : ViewModel() {
 
-    private val _selectedStamp = MutableLiveData<Int>()
-    val selectedStamp: LiveData<Int> get() = _selectedStamp
+    private val _homeEmotion = MutableLiveData<Int>()
+    val homeEmotion: LiveData<Int> get() = _homeEmotion
 
-//    init {
-//        homeResultViewModel.selectedStamp.observeForever { stamp ->
-//            _selectedStamp.value = when (stamp) {
-//                "a" -> R.drawable.home_emotion_happy
-//                "b" -> R.drawable.home_emotion_exciting
-//                "c" -> R.drawable.home_emotion_normal
-//                "d" -> R.drawable.home_emotion_anxiety
-//                "e" -> R.drawable.home_emotion_upset
-//                else -> R.drawable.home_emotion_none
-//            }
-//        }
-//    }
+    private val _text = MutableLiveData<String>()
+    val text: LiveData<String> get() = _text
+
+    private val _homeDateBackground = MutableLiveData<Int>()
+    val homeDateBackground: LiveData<Int> get() = _homeDateBackground
+
+    private val _homeBackground = MutableLiveData<Int>()
+    val homeBackground: LiveData<Int> get() = _homeBackground
+
+    fun updateHomeEmotion(emotion: Int, text: String, date: Int, background: Int) {
+        _homeEmotion.value = emotion
+        _text.value = text
+        _homeDateBackground.value = date
+        _homeBackground.value = background
+    }
 }
