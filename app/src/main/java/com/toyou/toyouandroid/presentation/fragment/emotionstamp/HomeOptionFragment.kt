f5b1c76e7b7a318997d6cc0951f48bf5ee9feeed
Daemon Jung <jjjssswww1006@gmail.com>
2024-07-19 15:55:32 +0900

    [feature] 감정우표 선택 후 viewmodel과 bundle로 데이터 전달 #13

--- app/src/main/java/com/toyou/toyouandroid/presentation/fragment/emotionstamp/HomeOptionFragment.kt
+++ app/src/main/java/com/toyou/toyouandroid/presentation/fragment/emotionstamp/HomeOptionFragment.kt
@@ -5,17 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.toyou.toyouandroid.R
 import com.toyou.toyouandroid.databinding.FragmentHomeOptionBinding
 import com.toyou.toyouandroid.presentation.base.MainActivity
 import com.toyou.toyouandroid.presentation.viewmodel.HomeOptionViewModel
+import com.toyou.toyouandroid.presentation.viewmodel.HomeViewModel
 
 class HomeOptionFragment : Fragment() {
 
+    lateinit var navController: NavController
     private var _binding: FragmentHomeOptionBinding? = null
     private val binding: FragmentHomeOptionBinding
         get() = requireNotNull(_binding){"FragmentHomeOptionBinding -> null"}
-    private val viewModel: HomeOptionViewModel by viewModels()
+    private val homeOptionViewModel: HomeOptionViewModel by activityViewModels()
+    private val homeViewModel: HomeViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,13 +29,96 @@ class HomeOptionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeOptionBinding.inflate(layoutInflater, container, false)
-        binding.viewModel = viewModel
+        binding.viewModel = homeOptionViewModel
         binding.lifecycleOwner = viewLifecycleOwner
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+
+        (requireActivity() as MainActivity).hideBottomNavigation(true)
+
+        binding.homeOptionBackBtn.setOnClickListener {
+            navController.navigate(R.id.action_navigation_home_option_to_home_fragment)
+        }
+
+        binding.homeStampOptionAnxietyIv.setOnClickListener {
+            val bundle = Bundle().apply {
+                putInt("background_color", R.drawable.background_green)
+                putString("text", getString(R.string.home_stamp_result_anxiety))
+            }
+
+            homeViewModel.updateHomeEmotion(
+                R.drawable.home_emotion_anxiety,
+                getString(R.string.home_emotion_anxiety_title),
+                R.color.g01,
+                R.drawable.background_green)
+
+            navController.navigate(R.id.action_navigation_home_option_to_home_result_fragment, bundle)
+        }
+
+        binding.homeStampOptionExcitingIv.setOnClickListener {
+            val bundle = Bundle().apply {
+                putInt("background_color", R.drawable.background_skyblue)
+                putString("text", getString(R.string.home_stamp_result_exciting))
+            }
+
+            homeViewModel.updateHomeEmotion(
+                R.drawable.home_emotion_exciting,
+                getString(R.string.home_emotion_exciting_title),
+                R.color.b01,
+                R.drawable.background_skyblue)
+
+            navController.navigate(R.id.action_navigation_home_option_to_home_result_fragment, bundle)
+        }
+
+        binding.homeStampOptionHappyIv.setOnClickListener {
+            val bundle = Bundle().apply {
+                putInt("background_color", R.drawable.background_yellow)
+                putString("text", getString(R.string.home_stamp_result_happy))
+            }
+
+            homeViewModel.updateHomeEmotion(
+                R.drawable.home_emotion_happy,
+                getString(R.string.home_emotion_happy_title),
+                R.color.y01,
+                R.drawable.background_yellow)
+
+            navController.navigate(R.id.action_navigation_home_option_to_home_result_fragment, bundle)
+        }
+
+        binding.homeStampOptionNormalIv.setOnClickListener {
+            val bundle = Bundle().apply {
+                putInt("background_color", R.drawable.background_purple)
+                putString("text", getString(R.string.home_stamp_result_normal))
+            }
+
+            homeViewModel.updateHomeEmotion(
+                R.drawable.home_emotion_normal,
+                getString(R.string.home_emotion_normal_title),
+                R.color.p01,
+                R.drawable.background_purple)
+
+            navController.navigate(R.id.action_navigation_home_option_to_home_result_fragment, bundle)
+        }
+
+        binding.homeStampOptionUpsetIv.setOnClickListener {
+            val bundle = Bundle().apply {
+                putInt("background_color", R.drawable.background_red)
+                putString("text", getString(R.string.home_stamp_result_upset))
+            }
+
+            homeViewModel.updateHomeEmotion(
+                R.drawable.home_emotion_upset,
+                getString(R.string.home_emotion_upset_title),
+                R.color.r01,
+                R.drawable.background_red)
+
+            navController.navigate(R.id.action_navigation_home_option_to_home_result_fragment, bundle)
+        }
+
     }
 
     override fun onDestroyView() {
