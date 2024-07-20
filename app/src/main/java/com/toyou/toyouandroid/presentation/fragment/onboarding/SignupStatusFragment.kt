667921b728dbce68fbd1ac46ceac433648004bd8
Daemon Jung <jjjssswww1006@gmail.com>
2024-07-20 11:12:18 +0900

    [feature] 회원가입 현재상태 화면 버튼 활성화 로직 추가 #13

--- app/src/main/java/com/toyou/toyouandroid/presentation/fragment/onboarding/SignupStatusFragment.kt
+++ app/src/main/java/com/toyou/toyouandroid/presentation/fragment/onboarding/SignupStatusFragment.kt
@@ -5,10 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.toyou.toyouandroid.R
-import com.toyou.toyouandroid.databinding.FragmentSignupnicknameBinding
 import com.toyou.toyouandroid.databinding.FragmentSignupstatusBinding
 import com.toyou.toyouandroid.presentation.base.MainActivity
 
@@ -18,6 +18,7 @@ class SignupStatusFragment : Fragment() {
     private var _binding: FragmentSignupstatusBinding? = null
     private val binding: FragmentSignupstatusBinding
         get() = requireNotNull(_binding){"FragmentSignupstatusBinding -> null"}
+    private val viewModel: SignupStatusViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,6 +27,8 @@ class SignupStatusFragment : Fragment() {
     ): View {
 
         _binding = FragmentSignupstatusBinding.inflate(inflater, container, false)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
 
         return binding.root
     }
@@ -39,8 +42,42 @@ class SignupStatusFragment : Fragment() {
         // navController 초기화
         navController = findNavController()
 
+        val buttonList = listOf(
+            binding.signupStatusOption1,
+            binding.signupStatusOption2,
+            binding.signupStatusOption3,
+            binding.signupStatusOption4
+        )
+
+        buttonList.forEach { button ->
+            button.setOnClickListener {
+                viewModel.onButtonClicked(button.id)
+                updateButtonBackgrounds()
+            }
+        }
+
+        viewModel.selectedButtonId.observe(viewLifecycleOwner) {
+            updateButtonBackgrounds()
+        }
+
+        viewModel.isNextButtonEnabled.observe(viewLifecycleOwner) { isEnabled ->
+            binding.signupStatusCompleteBtn.isEnabled = isEnabled
+        }
+
         binding.signupStatusCompleteBtn.setOnClickListener {
             navController.navigate(R.id.action_navigation_signup_status_to_home_fragment)
+        }
+    }
+
+    private fun updateButtonBackgrounds() {
+        val buttonList = listOf(
+            binding.signupStatusOption1,
+            binding.signupStatusOption2,
+            binding.signupStatusOption3,
+            binding.signupStatusOption4
+        )
+        buttonList.forEach { button ->
+            button.setBackgroundResource(viewModel.getButtonBackground(button.id))
         }
     }
 
