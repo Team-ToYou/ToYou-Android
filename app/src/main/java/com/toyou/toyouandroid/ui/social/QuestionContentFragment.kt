8dee0ebc33ac5bb4b26fc1b845d051a8da74e995
jeong724 <oliveyou724@gmail.com>
2024-07-24 03:24:39 +0900

    [feat] 질문 작성 글자수 제한

--- app/src/main/java/com/toyou/toyouandroid/ui/social/QuestionContentFragment.kt
+++ app/src/main/java/com/toyou/toyouandroid/ui/social/QuestionContentFragment.kt
@@ -1,9 +1,12 @@
 package com.toyou.toyouandroid.ui.social
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -16,6 +19,7 @@ class QuestionContentFragment : Fragment() {
     private val binding : FragmentQuestionContentBinding get() = requireNotNull(_binding){"널"}
 
     private lateinit var navController: NavController
+    private var questionEt : String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +40,7 @@ class QuestionContentFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
+        val wordCount: TextView = binding.limit200
 
         binding.nextBtn.setOnClickListener {
             navController.navigate(R.id.action_questionContentFragment_to_sendFragment)
@@ -44,5 +49,25 @@ class QuestionContentFragment : Fragment() {
             navController.popBackStack()
 
         }
+
+        binding.questionBoxEt.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+                wordCount.text = "(0/50)"
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                questionEt = s.toString()
+                wordCount.text = "${questionEt.length} / 50"
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                wordCount.text = "${questionEt.length} / 50"
+            }
+        })
     }
 }
