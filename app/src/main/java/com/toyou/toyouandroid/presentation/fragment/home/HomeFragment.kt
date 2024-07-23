61a932e7c4348de0321cf942ac743f2d38650bf0
Daemon Jung <jjjssswww1006@gmail.com>
2024-07-24 01:54:35 +0900

    [chore] 오타 수정 및 불필요한 코드 제거 #26

--- app/src/main/java/com/toyou/toyouandroid/presentation/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/toyou/toyouandroid/presentation/fragment/home/HomeFragment.kt
@@ -1,6 +1,5 @@
 package com.toyou.toyouandroid.presentation.fragment.home
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.MotionEvent
--- app/src/main/java/com/toyou/toyouandroid/presentation/viewmodel/HomeResultViewModel.kt
+++ app/src/main/java/com/toyou/toyouandroid/presentation/viewmodel/HomeResultViewModel.kt
@@ -1,29 +1,6 @@
 package com.toyou.toyouandroid.presentation.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.toyou.toyouandroid.di.FragmentNavigator
-import com.toyou.toyouandroid.model.HomeOptionResult
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
-class HomeResultViewModel (
-    private val navigator: FragmentNavigator
-) : ViewModel() {
-
-    private val _sentence = MutableLiveData<HomeOptionResult>()
-    val sentence: LiveData<HomeOptionResult> get() = _sentence
-
-    private val _selectedStamp = MutableLiveData<String>()
-    val selectedStamp: LiveData<String> get() = _selectedStamp
-
-    fun setSelectedStamp(stamp: String) {
-        _selectedStamp.value = stamp
-        viewModelScope.launch {
-            delay(2000)
-            navigator.navigateToHomeFragment()
-        }
-    }
+class HomeResultViewModel : ViewModel() {
 }
