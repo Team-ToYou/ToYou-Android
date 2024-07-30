b2e2cac6f4e15cacbc3a1f40d4649c1430174536
Daemon Jung <jjjssswww1006@gmail.com>
2024-07-30 20:59:49 +0900

    [feature] 기록 닉네임 글자수 제한 #30

--- app/src/main/java/com/toyou/toyouandroid/utils/BindingAdapters.kt
+++ app/src/main/java/com/toyou/toyouandroid/utils/BindingAdapters.kt
@@ -3,6 +3,7 @@ package com.toyou.toyouandroid.utils
 import android.graphics.BitmapFactory
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.LiveData
@@ -27,4 +28,11 @@ fun loadImage(view: ImageView, imagePath: String) {
 @BindingAdapter("android:setBackground")
 fun setBackground(button: AppCompatButton, resource: Int?) {
     resource?.let { button.setBackgroundResource(it) }
+}
+
+@BindingAdapter("formattedNickname")
+fun setFormattedNickname(textView: TextView, nickname: String?) {
+    nickname?.let {
+        textView.text = if (it.length > 3) "${it.substring(0, 3)}..." else it
+    }
 }
