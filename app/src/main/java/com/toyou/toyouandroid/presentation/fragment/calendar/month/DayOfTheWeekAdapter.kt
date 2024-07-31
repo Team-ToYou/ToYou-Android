91e332021f30b5f0607d03690b5c318a1f79f2e0
Daemon Jung <jjjssswww1006@gmail.com>
2024-07-31 23:59:23 +0900

    [feature] 캘린더 요일 어댑터 #14

---
+++ app/src/main/java/com/toyou/toyouandroid/presentation/fragment/calendar/month/DayOfTheWeekAdapter.kt
@@ -0,0 +1,27 @@
+package com.toyou.toyouandroid.presentation.fragment.calendar.month
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.toyou.toyouandroid.databinding.ItemDayoftheweekBinding
+
+class DayOfTheWeekAdapter(private val days: List<String>) : RecyclerView.Adapter<DayOfTheWeekAdapter.DayViewHolder>() {
+
+    class DayViewHolder(private val binding: ItemDayoftheweekBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(day: String) {
+            binding.dayTextOfWeek.text = day
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DayViewHolder {
+        val binding = ItemDayoftheweekBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return DayViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: DayViewHolder, position: Int) {
+        holder.bind(days[position])
+    }
+
+    override fun getItemCount(): Int = days.size
+
+}
