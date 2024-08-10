e56515ee7207f4a451eb3d5af5ab82d7932ebb97
jeong724 <oliveyou724@gmail.com>
2024-08-10 14:27:05 +0900

    [add] retrofit2 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,4 +71,9 @@ dependencies {
     implementation("com.jakewharton.timber:timber:4.7.1")
 
     implementation ("com.kakao.sdk:v2-all:2.20.3")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
 }
