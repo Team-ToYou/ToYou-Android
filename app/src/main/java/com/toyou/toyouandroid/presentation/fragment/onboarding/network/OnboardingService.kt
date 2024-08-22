838b05029fe0290e79df9cce7b898413ad8f2b0b
Daemon Jung <jjjssswww1006@gmail.com>
2024-08-22 09:58:40 +0900

    [feature] 닉네임 수정, 중복확인 api 설정 #35

---
+++ app/src/main/java/com/toyou/toyouandroid/presentation/fragment/onboarding/network/OnboardingService.kt
@@ -0,0 +1,21 @@
+package com.toyou.toyouandroid.presentation.fragment.onboarding.network
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.PATCH
+import retrofit2.http.Query
+
+interface OnboardingService {
+
+    @GET("users/nickname/check")
+    fun getNicknameCheck(
+        @Query("nickname") nickname: String
+    ): Call<NicknameCheckResponse>
+
+    @PATCH("users/nickname")
+    fun patchNickname(
+        @Header("userId") userId: Int,
+        @Query("nickname") nickname: PatchNicknameRequest
+    ): Call<PatchNicknameResponse>
+}
