0758d60f0b6bda08c607ac83545efba3f5fc20fb
Daemon Jung <jjjssswww1006@gmail.com>
2024-09-11 03:17:39 +0900

    [feature] 회원가입 데이터 클래스 정의 #35

---
+++ app/src/main/java/com/toyou/toyouandroid/presentation/fragment/onboarding/data/dto/request/SignUpRequest.kt
@@ -0,0 +1,7 @@
+package com.toyou.toyouandroid.presentation.fragment.onboarding.data.dto.request
+
+data class SignUpRequest(
+    val adConsent: Boolean,
+    val nickname: String,
+    val status: String
+)
---
+++ app/src/main/java/com/toyou/toyouandroid/presentation/fragment/onboarding/data/dto/response/SignUpResponse.kt
@@ -0,0 +1,7 @@
+package com.toyou.toyouandroid.presentation.fragment.onboarding.data.dto.response
+
+data class SignUpResponse(
+    val isSuccess: Boolean,
+    val code: String,
+    val message: String
+)
