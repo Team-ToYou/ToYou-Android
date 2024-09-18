11a040f777a89a1cce43932124d8cdbcff08b275
Daemon Jung <jjjssswww1006@gmail.com>
2024-09-19 03:20:45 +0900

    [chore] 마이페이지 조회 api 수정 #35

--- app/src/main/java/com/toyou/toyouandroid/presentation/fragment/mypage/network/MypageService.kt
+++ app/src/main/java/com/toyou/toyouandroid/presentation/fragment/mypage/network/MypageService.kt
@@ -2,12 +2,9 @@ package com.toyou.toyouandroid.presentation.fragment.mypage.network
 
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Header
 
 interface MypageService {
 
     @GET("/users/mypage")
-    fun getMypage(
-        @Header("userId") userId: Int
-    ): Call<MypageResponse>
+    fun getMypage(): Call<MypageResponse>
 }
