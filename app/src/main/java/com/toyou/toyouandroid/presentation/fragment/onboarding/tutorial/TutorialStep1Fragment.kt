6bf19e888ca47635f577723b8dd1cc36cc7d3938
Daemon Jung <jjjssswww1006@gmail.com>
2024-09-27 03:02:52 +0900

    [feature] 튜토리얼 프래그먼트 #52

---
+++ app/src/main/java/com/toyou/toyouandroid/presentation/fragment/onboarding/tutorial/TutorialStep1Fragment.kt
@@ -0,0 +1,52 @@
+package com.toyou.toyouandroid.presentation.fragment.onboarding.tutorial
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.toyou.toyouandroid.presentation.base.MainActivity
+import com.toyou.toyouandroid.databinding.FragmentTutorialStep1Binding
+
+class TutorialStep1Fragment : Fragment() {
+
+    private lateinit var navController: NavController
+    private var _binding: FragmentTutorialStep1Binding? = null
+    private val binding: FragmentTutorialStep1Binding
+        get() = requireNotNull(_binding){"FragmentTutorialStep1Binding -> null"}
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+
+        _binding = FragmentTutorialStep1Binding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // MainActivity의 메소드를 호출하여 바텀 네비게이션 뷰 숨기기
+        (requireActivity() as MainActivity).hideBottomNavigation(true)
+
+        // navController 초기화
+        navController = Navigation.findNavController(view)
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                requireActivity().finish()
+            }
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
