b531bb98d130fa6f4a50c45fc7d9412b1731db42
Daemon Jung <jjjssswww1006@gmail.com>
2024-09-27 03:06:23 +0900

    [feature] 튜토리얼 설정 #52

---
+++ app/src/main/java/com/toyou/toyouandroid/presentation/fragment/onboarding/tutorial/TutorialVPAdapter.kt
@@ -0,0 +1,24 @@
+package com.toyou.toyouandroid.presentation.fragment.onboarding.tutorial
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+class TutorialVPAdapter(fragmentManager: FragmentManager) : FragmentStatePagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> TutorialStep1Fragment()
+            1 -> TutorialStep2Fragment()
+            2 -> TutorialStep3Fragment()
+            3 -> TutorialStep4Fragment()
+            4 -> TutorialStep5Fragment()
+            else -> TutorialStep1Fragment()
+        }
+    }
+
+    override fun getCount(): Int {
+        // Fragment의 개수
+        return 5
+    }
+}
