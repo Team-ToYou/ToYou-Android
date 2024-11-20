5a76d7fea3578dee0ac1bb15b75992b9357426d2
Daemon Jung <jjjssswww1006@gmail.com>
2024-11-20 11:34:06 +0900

    [feature] 알림 설정 토스트 메시지 추가

--- app/src/main/java/com/toyou/toyouandroid/presentation/fragment/mypage/NoticeSettingFragment.kt
+++ app/src/main/java/com/toyou/toyouandroid/presentation/fragment/mypage/NoticeSettingFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -60,9 +61,11 @@ class NoticeSettingFragment : Fragment() {
             if (isChecked) {
                 myFirebaseMessagingService.subscribeToTopic()
                 Timber.d("구독됨")
+                Toast.makeText(context, "알림 수신을 동의하였습니다", Toast.LENGTH_SHORT).show()
             } else {
                 myFirebaseMessagingService.unsubscribeFromTopic()
                 Timber.d("구독 취소됨")
+                Toast.makeText(context, "알림 수신을 거부하였습니다", Toast.LENGTH_SHORT).show()
             }
 
             sharedPreferences.edit().putBoolean("isSubscribed", isChecked).apply()
