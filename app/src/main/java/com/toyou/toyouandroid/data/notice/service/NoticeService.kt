39aae85210b3a072b6485a689b288a83a869cd14
Daemon Jung <jjjssswww1006@gmail.com>
2025-02-26 22:06:50 +0900

    [hotfix] 알림 수락 서버 로직 변경사항 반영

--- app/src/main/java/com/toyou/toyouandroid/data/notice/service/NoticeService.kt
+++ app/src/main/java/com/toyou/toyouandroid/data/notice/service/NoticeService.kt
@@ -2,6 +2,7 @@ package com.toyou.toyouandroid.data.notice.service
 
 import com.toyou.toyouandroid.data.notice.dto.AlarmDeleteResponse
 import com.toyou.toyouandroid.data.notice.dto.AlarmResponse
+import com.toyou.toyouandroid.data.notice.dto.FriendsRequestResponse
 import retrofit2.Call
 import retrofit2.http.DELETE
 import retrofit2.http.GET
@@ -13,4 +14,7 @@ interface NoticeService {
 
     @DELETE("alarms/{id}")
     fun deleteAlarm(@Path("id") id: Int): Call<AlarmDeleteResponse>
+
+    @GET("/friends/requests")
+    fun getFriendsRequest(): Call<FriendsRequestResponse>
 }
