68c1048647785e9152d06244f07be1128d5cb46f
Daemon Jung <jjjssswww1006@gmail.com>
2025-02-26 22:16:26 +0900

    [release] 투유 1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.toyou.toyouandroid"
         minSdk = 28
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
