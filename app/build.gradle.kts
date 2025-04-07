5aa0f6b33d232eb463c6d0918e381dea5fba35a0
Daemon Jung <jjjssswww1006@gmail.com>
2025-04-07 21:11:40 +0900

    [release] v1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 11
-        versionName = "1.0.10"
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
